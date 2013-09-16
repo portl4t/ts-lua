04d5effbd178d6611a1d2eafe807641b08bfb24b
portl4t <portl4t.cn@gmail.com>
2013-09-16 09:09:28 -0400

    inject_misc api

--- src/ts_lua_util.c
+++ src/ts_lua_util.c
@@ -4,6 +4,7 @@
 #include "ts_lua_client_response.h"
 #include "ts_lua_context.h"
 #include "ts_lua_hook.h"
+#include "ts_lua_misc.h"
 
 static lua_State * ts_lua_new_state();
 static void ts_lua_init_registry(lua_State *L);
@@ -143,6 +144,8 @@ ts_lua_inject_ts_api(lua_State *L)
 
     ts_lua_inject_context_api(L);
     ts_lua_inject_hook_api(L);
+
+    ts_lua_inject_misc_api(L);
 
     lua_getglobal(L, "package");
     lua_getfield(L, -1, "loaded");
