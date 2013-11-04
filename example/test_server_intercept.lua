87883d61a52902a2833f4901c18c86002d2fcc32
portl4t <portl4t.cn@gmail.com>
2013-11-04 06:38:31 -0500

    modify intercept example

--- example/test_server_intercept.lua
+++ example/test_server_intercept.lua
@@ -1,6 +1,8 @@
 
+require 'os'
+
 function send_data()
-    local nt = os.time()..' Zheng.\n'
+    local nt = os.time()..'\n'
 
     local resp =  'HTTP/1.1 200 OK\r\n' ..
         'Server: ATS/3.2.0\r\n' ..
@@ -8,17 +10,16 @@ function send_data()
         'Content-Length: ' .. string.len(nt) .. '\r\n' ..
         'Last-Modified: ' .. os.date("%a, %d %b %Y %H:%M:%S GMT", os.time()) .. '\r\n' ..
         'Connection: keep-alive\r\n' ..
-        'Cache-Control: max-age=7200\r\n' ..
+        'Cache-Control: max-age=30\r\n' ..
         'Accept-Ranges: bytes\r\n\r\n' ..
         nt
 
-    ts.sleep(1)
     return resp
 end
 
 
 function do_remap()
-    ts.http.intercept(send_data)
+    ts.http.server_intercept(send_data)
     return 0
 end
 
