568bdf2937f5ba9d65b734804670d262a15f76c5
Quehan <quehan@taobao.com>
2013-12-31 20:34:53 +0800

    add ts.md5

--- src/ts_lua_util.c
+++ src/ts_lua_util.c
@@ -31,6 +31,7 @@
 #include "ts_lua_misc.h"
 #include "ts_lua_regex.h"
 #include "ts_lua_log.h"
+#include "ts_lua_crypto.h"
 
 static lua_State * ts_lua_new_state();
 static void ts_lua_init_registry(lua_State *L);
@@ -250,6 +251,7 @@ ts_lua_inject_ts_api(lua_State *L)
     ts_lua_inject_http_api(L);
     ts_lua_inject_misc_api(L);
     ts_lua_inject_regex_api(L);
+    ts_lua_inject_crypto_api(L);
 
     lua_getglobal(L, "package");
     lua_getfield(L, -1, "loaded");
